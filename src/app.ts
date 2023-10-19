608be8bc5bdffd52c2ee79e13117665957fd42d3
zahidmiran <zahidmiron599@gmail.com>
2023-10-19 20:37:37 +0600

    backend deployed to vercel

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,7 @@ const app: Application = express();
 // app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(
   cors({
-    origin:
-      'https://excellence-builders-backend-m7hc5m419-jawadjisan.vercel.app',
+    origin: 'https://excellence-builders-backend.vercel.app',
     credentials: true,
   })
 );

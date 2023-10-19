5bfa12090df5624acd3742069adc17274bf211b2
zahidmiran <zahidmiron599@gmail.com>
2023-10-20 00:08:00 +0600

    cors link updated

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 // app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(
   cors({
-    origin: 'https://excellence-builders-backend.vercel.app',
+    origin: 'https://excellence-builders-frontend.vercel.app',
     credentials: true,
   })
 );
